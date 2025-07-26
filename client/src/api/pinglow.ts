85d670f983d315623ffe446404aecd3afd4ea9b8
Davide Sbetti <davide.sbetti@gmail.com>
2025-07-26 22:32:28 +0200

    Fix call to backend

--- client/src/api/pinglow.ts
+++ client/src/api/pinglow.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { Check, CheckWithStatus } from '@/types/Check'
 import type { PerformanceData } from '@/types/PerformanceData';
 
-const axiosInstance = axios.create({ baseURL: "http://localhost/api" });
+const axiosInstance = axios.create({ baseURL: "/api" });
 
 export async function getChecks(): Promise<Check[]> {
     const response = await axiosInstance.get<Check[]>('/checks');
