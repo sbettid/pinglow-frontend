6dcc4ed2f10b9f7339e3146347101ad2787749a7
Davide Sbetti <davide.sbetti@gmail.com>
2025-07-26 20:44:20 +0200

    Fix base url in vue js

--- client/src/api/pinglow.ts
+++ client/src/api/pinglow.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { Check, CheckWithStatus } from '@/types/Check'
 import type { PerformanceData } from '@/types/PerformanceData';
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:8080/api" });
+const axiosInstance = axios.create({ baseURL: "http://localhost:80/api" });
 
 export async function getChecks(): Promise<Check[]> {
     const response = await axiosInstance.get<Check[]>('/checks');
