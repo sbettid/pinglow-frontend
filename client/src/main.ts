b2dbe0159d122c33f9ba5ae3d537e20af4163a19
Davide Sbetti <davide.sbetti@gmail.com>
2025-10-19 09:55:24 +0200

    fix: remove unnecessary comment

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,7 +6,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import "vuetify/styles/main.css"
-import '@mdi/font/css/materialdesignicons.css'  // <- ✅ This is the missing piece
+import '@mdi/font/css/materialdesignicons.css'
 
 const vuetify = createVuetify({
     components,
